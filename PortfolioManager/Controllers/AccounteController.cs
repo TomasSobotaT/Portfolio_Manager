08472af2a22a8cfc9dbe25f09bb29b0c6f90e378
Tomas Sobota <t.sobota@volny.cz>
2023-07-23 22:21:10 +0200

    Register actions added to controller, register view, registration and login works

--- PortfolioManager/Controllers/AccounteController.cs
+++ PortfolioManager/Controllers/AccounteController.cs
@@ -13,24 +13,27 @@ namespace PortfolioManager.Controllers
 
         private readonly SignInManager<IdentityUser> signInManager;
         private readonly UserManager<IdentityUser> userManager;
+		private readonly ILogger<HomeController> _logger;
 
 
-        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
-        {
-            this.signInManager = signInManager;
-            this.userManager = userManager;
-        }
+		public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, ILogger<HomeController> logger)
+		{
+			this.signInManager = signInManager;
+			this.userManager = userManager;
+			_logger = logger;
+		}
 
 
 
 
-        [HttpGet]
+		[HttpGet]
         public IActionResult Login(string? returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
 
             return View();
         }
+
 
         /// <summary>
         /// Login through post form
@@ -52,19 +55,60 @@ namespace PortfolioManager.Controllers
                 loginViewModel.Email,loginViewModel.Password,loginViewModel.RememberMe,lockoutOnFailure:false);
 
 
-            if(result.Succeeded)
-            {
-                if(returnUrl == null)
-                   RedirectToAction("Index", "Home");   // ´
+			if (result.Succeeded)
+				return RedirectToLocal(returnUrl);
+			
 
-                RedirectToAction(returnUrl);
 
-            }
-            ModelState.AddModelError("", "Neplatné přihlašovací údaje");
+			ModelState.AddModelError("", "Neplatné přihlašovací údaje");
             return View(loginViewModel);
         
         }
 
+
+		[HttpGet]
+		[AllowAnonymous]
+		public IActionResult Register(string? returnUrl = null)
+		{
+			ViewData["ReturnUrl"] = returnUrl;
+
+			return View();
+		}
+
+		/// <summary>
+		/// Register through post form
+		/// </summary>
+		/// <param name="registerViewModel"></param>
+		/// <param name="returnUrl"> parameter ?returnUrl= </param>
+		/// <returns></returns>
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Register(RegisterViewModel registerViewModel, string? returnUrl = null)
+		{
+			ViewData["ReturnUrl"] = returnUrl;
+
+
+            if (!ModelState.IsValid)
+                return View(registerViewModel);
+
+			
+				var user = new IdentityUser { UserName = registerViewModel.Email, Email = registerViewModel.Email };
+				var result = await userManager.CreateAsync(user, registerViewModel.Password);
+
+				if (result.Succeeded)
+				{
+					await signInManager.SignInAsync(user, isPersistent: false);
+					    return RedirectToLocal(returnUrl);
+				}
+
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError("", error.Description);
+				}
+			
+
+			return View(registerViewModel);
+		}
 
 
         /// <summary>
--- PortfolioManager/Models/AccountViewModels/LoginViewModel.cs
+++ PortfolioManager/Models/AccountViewModels/LoginViewModel.cs
@@ -10,7 +10,7 @@ namespace PortfolioManager.Models.AccountViewModels
         public string Email { get; set; } = "";
 
         [Required]
-        [StringLength(30, ErrorMessage = "Zadejte platné heslo ({2} až {1} znaků)", MinimumLength = 6)]
+        [StringLength(30, ErrorMessage = "Zadejte platné heslo", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Heslo")]
         public string Password { get; set; } = "";
